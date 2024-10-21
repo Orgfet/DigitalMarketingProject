d6770c560c66c5fd4d8065f3be4ee3641fbac8d7
Felix Tippe <tippefelix@gmail.com>
2024-10-21 15:46:02 +0200

    added about,contact and css files

--- code/GeneralPageLogic.js
+++ code/GeneralPageLogic.js
@@ -1,4 +1,10 @@
 function LoadDarkmode() {
-
-
+    const body = document.body;
+    body.classList.toggle('dark-mode');
 }
+
+// Listener
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    darkModeToggle.addEventListener('click', LoadDarkmode);
+});
